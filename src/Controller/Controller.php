bfd8c1d3e5e25d27205ef81ca9deef7a2e28bfdb
Ivan Barlog <ivanbarlog@users.noreply.github.com>
2017-09-05 00:48:23 +0200

    Update Controller.php

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace AppBundle\Controller;
+namespace Everlution\AjaxcomBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as SymfonyController;
 
 /**
- * Class BaseController.
+ * Class Controller.
  *
  * @author Ivan Barlog <ivan.barlog@everlution.sk>
  */
