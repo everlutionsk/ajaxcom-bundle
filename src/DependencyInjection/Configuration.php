3b9fcb0fd4f25513094fa00c3c5161703bc7cca4
Peter Novotnicek <novotnicek.peter@gmail.com>
2021-08-18 19:28:38 +0200

    fix deprecated configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -23,8 +23,14 @@ class Configuration implements ConfigurationInterface
 
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('everlution_ajaxcom');
+        $treeBuilder = new TreeBuilder('everlution_ajaxcom');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('everlution_ajaxcom');
+        }
 
         $rootNode
             ->children()
